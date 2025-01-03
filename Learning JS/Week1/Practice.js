7d22d3d128bfbed8708377bd55a5d5cb979518ea
DivPandey <divyanshu1106@gmail.com>
2025-01-03 20:08:47 +0530

    Learning js

--- Learning JS/Week1/Practice.js
+++ Learning JS/Week1/Practice.js
@@ -2,3 +2,17 @@ console.log("Hello World")
 console.log("How are You")
 
 console.log("ans the following questions")
+
+var a = 1;
+console.log(a)
+
+let Firstname = "Divyanshu";
+let age = 19;
+let isMarried = true;
+console.log("This person name is "+Firstname+ " and their age is "+ age);
+
+if(isMarried==true){
+    console.log("This person is married");
+}else{
+    console.log("This person is not married");
+}
---
+++ Learning JS/Week1/w12.js
@@ -0,0 +1,17 @@
+let firstName="Divyanshu";
+let age=19;
+let isMarried=true;
+
+console.log("This persons name is "+firstName+" and thier age is "+age);
+
+if(age>18){
+    console.log("THis person "+firstName+" is an adult");
+}else{
+    console.log("THis person is teen");
+}
+
+if(isMarried){
+    console.log("This person is married");
+}else{
+    console.log("thsi person is unmarried");
+}
